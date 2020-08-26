b44b069169c13cc4e343be343eafa780e7a09980
ZamranxD <zamran.butt.1@gmail.com>
2020-08-26 08:41:21 +0500

    capture first frame of video

---
+++ capture.py
@@ -0,0 +1,15 @@
+import cv2, time
+
+
+video = cv2.VideoCapture(0)
+
+check, frame = video.read()
+
+print(check)
+print(frame)
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Meee", gray)
+cv2.waitKey(0)
+video.release()
+cv2.destroyAllWindows
